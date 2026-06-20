cc6bd6296332c02ddc1b2190ec5fea7d208385f5
Raveenfx <m.rafippp14@gmail.com>
2026-06-21 06:22:30 +0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,4 +3,5 @@ import os
 class Config:
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024  
     ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
-    MODEL_PATH = os.environ.get('MODEL_PATH', 'path/to/your/model.h5')
+    BASE_DIR = os.getcwd()
+    MODEL_PATH = os.environ.get('MODEL_PATH', os.path.join(BASE_DIR, 'model', 'saved_model', 'freshscan.h5'))
